c067c52176c329be53b303942e11b2927796ef20
sareh <sareh.heidari@gmail.com>
2015-10-31 18:56:18 +0000

    Updated config/routes.rb. Changed root, added users pages & redirected tagged pages

--- portal/config/routes.rb
+++ portal/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  
+  root "services#index"
+  
   resources :services
   resources :service_types
   devise_for :users
+  resources :users, only: [:index, :new, :show, :edit, :delete]
+
+  get 'tagged' => 'services#tagged', :as => 'tagged'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
