c0ce958ef22ff21b9ee74d8544c64024bd4a1346
sareh <sareh.heidari@gmail.com>
2015-11-02 14:00:20 +0000

    Updated TAGS constant array

--- portal/app/models/tag.rb
+++ portal/app/models/tag.rb
@@ -1,2 +1,24 @@
 class Tag < ActiveRecord::Base
+  TAGS = ["mental health",
+          "physical health",
+          "academic department",
+          "finance",
+          "housing",
+          "support groups",
+          "liberation networks",
+          "timetable",
+          "careers",
+          "visas",
+          "transport",
+          "academic support",
+          "interruption of studies",
+          "postgraduate student",
+          "undergraduate student",
+          "research student",
+          "parents & carers",
+          "mature student",
+          "studying abroad",
+          "insurance",
+          "isd"
+         ]
 end
