5ede6cfb266cd7de3db57ef680ff37ddf0a18262
sareh <sareh.heidari@gmail.com>
2015-11-04 09:39:38 +0000

    Added validation on service variables

--- portal/app/models/service.rb
+++ portal/app/models/service.rb
@@ -3,6 +3,13 @@ class Service < ActiveRecord::Base
   belongs_to :service_type
   acts_as_taggable_on :tags
 
+  validates :name,        length: { minimum: 2 }
+  validates :url,         length: { minimum: 3 }
+  validates :description, length: { minimum: 20 }
+  validates :user_id,         presence: true
+  validates :service_type_id, presence: true
+  # validates :image, length: { minimum: 3 }
+
   def self.unique_tags
     Tag.all.uniq
   end
