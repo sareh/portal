6cf56978f94af1ccc8d9cf5dc5c70fe9f48fe3cc
sareh <sareh.heidari@gmail.com>
2015-11-05 08:00:05 +0000

    Added validations - currently commented out, until testing

--- portal/app/models/user.rb
+++ portal/app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :services
+
+  # validates :username,         length: { minimum: 2 }
+  # validates :role,             presence: true
+  # validates :email,            length: { minimum: 3 }
+  # validates :password,         length: { minimum: 8 }
+  # validates :confirm_password, length: { minimum: 8 }
+  
+
 end
