085ae983bc5e142fc903383142bf518048f4539d
sareh <sareh.heidari@gmail.com>
2015-11-05 19:05:22 +0000

    Initialiser file for connecting to AWS

--- portal/config/initializers/fog.rb
+++ portal/config/initializers/fog.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
+  # config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     'AKIAIP62HDMWZISLBERQ',       # required
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
     host:                  's3.example.com',             # optional, defaults to nil
     endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'name_of_directory'                          # required
+  config.fog_directory  = 'portal'                       # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
