fc48ceed9a8813287a499f46c277434e7b6dd440
sareh <sareh.heidari@gmail.com>
2015-11-05 18:19:19 +0000

    Initialiser links with image_tag helper

---
+++ portal/config/initializers/fog.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     'AKIAIP62HDMWZISLBERQ',       # required
+    aws_secret_access_key: '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', # required
+    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+    host:                  's3.example.com',             # optional, defaults to nil
+    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'name_of_directory'                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
